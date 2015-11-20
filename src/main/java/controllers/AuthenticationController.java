91cbec3a542ba48d55fdcb745365bc4a9042d794
djastin <djastingvdamme@hotmail.com>
2015-11-20 16:38:11 +0100

    Initialize Webapp

---
+++ src/main/java/controllers/AuthenticationController.java
@@ -0,0 +1,20 @@
+package controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+public class AuthenticationController
+{
+	@RequestMapping("/signin")
+	public ModelAndView showMessage(
+			@RequestParam(value = "name", required = false, defaultValue = "World") String name)
+	{ 
+		ModelAndView mv = new ModelAndView("login");
+		mv.addObject("message", "");
+		mv.addObject("name", name);
+		return mv;
+	}
+}
